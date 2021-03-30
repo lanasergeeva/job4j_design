923085f6cca38c129ea29814cf704529df1a89fd
PoymaLana <marikakopina@gmail.com>
2021-03-30 14:13:56 +0300

    2. Статистику по коллекции. [#477974]

---
+++ src/main/java/ru/job4j/collection/Analize.java
@@ -0,0 +1,93 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class Analize {
+    public Info diff(List<User> previous, List<User> current) {
+        Map<Integer, String> map = new HashMap<>();
+        Info info = new Info();
+        for (User user : previous) {
+            map.put(user.getId(), user.getName());
+        }
+        for (User user : current) {
+            if (!user.getName().equals(map.get(user.getId()))) {
+                info.changed++;
+            }
+        }
+        if (map.keySet().size() < current.size()) {
+            info.added = current.size() - map.keySet().size();
+            info.changed = info.changed - info.added;
+        } else if (map.keySet().size() > current.size()) {
+            info.deleted = map.keySet().size() - current.size();
+        }
+        return info;
+    }
+
+
+    public static class User {
+        int id;
+        String name;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            User user = (User) o;
+            return id == user.id && Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
+    }
+
+    public static class Info {
+
+        int added;
+        int changed;
+        int deleted;
+
+        public int getAdded() {
+            return added;
+        }
+
+        public int getChanged() {
+            return changed;
+        }
+
+        public int getDeleted() {
+            return deleted;
+        }
+
+    }
+}
+
---
+++ src/test/java/ru/job4j/collection/AnalizeTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class AnalizeTest {
+    Analize.User user1 = new Analize.User(10, "Oleg");
+    Analize.User user2 = new Analize.User(15, "Nikita");
+    Analize.User user3 = new Analize.User(20, "Igor");
+    Analize.User user4 = new Analize.User(20, "Tim");
+    Analize.User user5 = new Analize.User(25, "Uriy");
+    Analize.User user6 = new Analize.User(26, "Ratmir");
+    Analize.User user7 = new Analize.User(27, "Michel");
+    Analize.User user8 = new Analize.User(27, "Nazar");
+
+    @Test
+    public void whenTheSame() {
+        List<Analize.User> list = new ArrayList<>(Arrays.asList(user1, user2, user3, user5));
+        List<Analize.User> check = new ArrayList<>(Arrays.asList(user1, user2, user3, user5));
+        Analize.Info info = new Analize().diff(list, check);
+        assertThat(info.getAdded(), is(0));
+        assertThat(info.getDeleted(), is(0));
+        assertThat(info.getChanged(), is(0));
+    }
+
+    @Test
+    public void whenAdd() {
+        List<Analize.User> list = new ArrayList<>(Arrays.asList(user1, user2, user3));
+        List<Analize.User> check = new ArrayList<>(Arrays.asList(user1, user2, user3, user5, user6, user7));
+        Analize.Info info = new Analize().diff(list, check);
+        assertThat(info.getAdded(), is(3));
+        assertThat(info.getDeleted(), is(0));
+        assertThat(info.getChanged(), is(0));
+    }
+
+    @Test
+    public void whenAddAndChange() {
+        List<Analize.User> list = new ArrayList<>(Arrays.asList(user1, user2, user3, user7));
+        List<Analize.User> check = new ArrayList<>(Arrays.asList(user1, user2, user4, user5, user8));
+        Analize.Info info = new Analize().diff(list, check);
+        assertThat(info.getAdded(), is(1));
+        assertThat(info.getDeleted(), is(0));
+        assertThat(info.getChanged(), is(2));
+    }
+
+    @Test
+    public void whenDelete() {
+        List<Analize.User> list = new ArrayList<>(Arrays.asList(user1, user2, user3, user5, user6, user7));
+        List<Analize.User> check = new ArrayList<>(Arrays.asList(user2, user3));
+        Analize.Info info = new Analize().diff(list, check);
+        assertThat(info.getAdded(), is(0));
+        assertThat(info.getDeleted(), is(4));
+        assertThat(info.getChanged(), is(0));
+    }
+}
