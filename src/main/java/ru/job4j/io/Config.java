a90ee7db96fcd708df9fbf81956778e7a96fa514
PoymaLana <marikakopina@gmail.com>
2021-04-01 13:36:33 +0300

    1. Читаем файл конфигурации [#478035] исправила

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -4,11 +4,10 @@ import java.io.IOException;
 import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.stream.Collectors;
 
 public class Config {
     private final String path;
-    private final Map<String, String> values = new HashMap<String, String>();
+    private final Map<String, String> values = new HashMap<>();
 
     public Config(final String path) {
         this.path = path;
@@ -16,17 +15,16 @@ public class Config {
 
     public void load() {
         try (BufferedReader in = new BufferedReader(new FileReader(path))) {
-            String[] array = null;
-            List<String> rsl = in.lines()
-                    .filter(line -> !line.isEmpty() && !line.startsWith("#"))
-                    .collect(Collectors.toList());
-            for (String s : rsl) {
-                array = s.split("=", 2);
-                if (array[1].isEmpty()) {
-                    throw new IllegalArgumentException();
-                }
-                values.put(array[0], array[1]);
-            }
+            in.lines()
+                    .filter(line -> !line.isEmpty() && !line.startsWith("#") && line.contains("="))
+                    .forEach(line -> {
+                            String[] array = line.split("=", 2);
+                            if (array[1].isEmpty()) {
+                                throw new IllegalArgumentException();
+                            } else {
+                                values.put(array[0], array[1]);
+                        }
+                    });
         } catch (IOException e) {
             e.printStackTrace();
         }
