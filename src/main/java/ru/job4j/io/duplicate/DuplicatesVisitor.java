85b0e4852678cd167e98328bfc2c795979709f2e
PoymaLana <marikakopina@gmail.com>
2021-04-03 23:46:25 +0300

    4.2. Поиск дубликатов [#478034]

---
+++ src/main/java/ru/job4j/io/duplicate/DuplicatesVisitor.java
@@ -0,0 +1,37 @@
+package ru.job4j.io.duplicate;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private Set<FileProperty> files = new HashSet<>();
+    private Map<FileProperty, Path> map = new HashMap<>();
+
+   /* @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty fileProperty = new FileProperty(file.toFile().length(), *//*file.getFileName().toString()*//* file.toFile().getName());
+        if (files.contains(fileProperty)) {
+            System.out.println(file.toAbsolutePath());
+        } else {
+            files.add(fileProperty);
+        }
+        return FileVisitResult.CONTINUE;
+    }*/
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty fileProperty = new FileProperty(file.toFile().length(), file.toFile().getName());
+        if (map.containsKey(fileProperty)) {
+            System.out.println("First duplicate path is " + map.get(fileProperty));
+            System.out.println("Second duplicate path is  " + file.toAbsolutePath());
+        } else {
+            map.put(fileProperty, file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
