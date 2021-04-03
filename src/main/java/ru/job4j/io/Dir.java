8efef022888df7f0f3135471e30cc8fa6ae335e6
PoymaLana <marikakopina@gmail.com>
2021-04-04 00:08:01 +0300

    небольшие коррективы

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -1,4 +1,5 @@
 package ru.job4j.io;
+
 import java.io.File;
 import java.io.IOException;
 
--- src/main/java/ru/job4j/io/duplicate/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicate/DuplicatesVisitor.java
@@ -14,7 +14,7 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
    /* @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        FileProperty fileProperty = new FileProperty(file.toFile().length(), *//*file.getFileName().toString()*//* file.toFile().getName());
+        FileProperty fileProperty = new FileProperty(file.toFile().length(), file.toFile().getName());
         if (files.contains(fileProperty)) {
             System.out.println(file.toAbsolutePath());
         } else {
