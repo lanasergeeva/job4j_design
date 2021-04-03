b082a4511e4736064ca173956a4273d4b62d9e3a
PoymaLana <marikakopina@gmail.com>
2021-04-04 00:02:45 +0300

    3.0. Тестирование IO [#478027] через Set

--- src/main/java/ru/job4j/io/Abuse.java
+++ src/main/java/ru/job4j/io/Abuse.java
@@ -1,15 +1,19 @@
 package ru.job4j.io;
+
 import java.io.*;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public class Abuse {
     public static void drop(String source, String target, List<String> words) throws IOException {
         try (BufferedReader in = new BufferedReader(new FileReader(source));
              PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            Set<String> setWords = new HashSet<>(words);
             in.lines()
                     .flatMap(line -> Stream.of(line.split("\\s+")))
-                    .filter(word -> !words.contains(word)).map(word -> word + " ")
+                    .filter(word -> !setWords.contains(word)).map(word -> word + " ")
                     .forEach(out::print);
         }
     }
