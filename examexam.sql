6e14855ab7a80301b6372be8fd8b03a504ce456d
lanasergeeva <75443532+lanasergeeva@users.noreply.github.com>
2021-06-18 20:05:59 +0300

    aaa

--- examexam.sql
+++ examexam.sql
@@ -15,6 +15,35 @@ meeting_id integer references meeting(id),
 status varchar check (status = 'accept' or status = 'deny')
 );
 
+delete from potential_participants;
+
+insert into potential_participants
+values(1, 1, 1, 'accept');
+insert into potential_participants
+values(2, 2, 1, 'deny');
+insert into potential_participants
+values(3, 3, 1, 'deny');
+insert into potential_participants
+values(4, 4, 1, 'accept');
+insert into potential_participants
+values(5, 1, 2, 'deny');
+insert into potential_participants
+values(6, 2, 2, 'deny');
+insert into potential_participants
+values(7, 3, 2, 'deny');
+insert into potential_participants
+values(8, 4, 2, 'deny');
+insert into potential_participants
+values(9, 1, 3, 'accept');
+insert into potential_participants
+values(10, 2, 3, 'deny');
+insert into potential_participants
+values(11, 3, 3, 'accept');
+insert into potential_participants
+values(12, 4, 3, 'accept');
+
+select * from potential_participants;
+
 select  m.name, count(*) from 
 potential_participants p
 join meeting m
@@ -22,11 +51,13 @@ on (p.meeting_id = m.id)
 group by m.name, meeting_id, status
 having status = 'accept';
 
-select m.name, count(*) from meeting m 
+select m.name, count(*) 
+from meeting m 
 join potential_participants p
 on(m.id=p.meeting_id)
-group by  m.name, p.users_id
-having count(*) = 0 or
- p.users_id is null;
-
+where m.name = (
+select name from meeting where id=2 ) 
+and 
+p.status = 'deny'
+group by  m.name;
 
