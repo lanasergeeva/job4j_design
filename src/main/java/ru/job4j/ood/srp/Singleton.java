e63aa41d9006f155027594a18cf02a85ce6589c2
PoymaLana <marikakopina@gmail.com>
2021-08-10 18:51:21 +0300

    SRP первый пример

---
+++ src/main/java/ru/job4j/ood/srp/Singleton.java
@@ -0,0 +1,16 @@
+package ru.job4j.ood.srp;
+
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
