7e22e96dee768424291fbdba8eacf0cbcc80be09
PoymaLana <marikakopina@gmail.com>
2021-08-10 15:16:28 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.template;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.util.*;
+
+public class GeneratorTest {
+
+    @Test
+    public void whenCorrect() {
+        String test = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "David");
+        map.put("subject", "you");
+        Temp temp = new Temp();
+        String rsl = "I am a David, Who are you?";
+        assertEquals(rsl, temp.produce(test, map));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenIncorrectTemplate() {
+        String test = "I am a ${name}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "David");
+        map.put("subject", "you");
+        Temp temp = new Temp();
+        temp.produce(test, map);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenIncorrectKeys() {
+        String test = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "David");
+        Temp temp = new Temp();
+        temp.produce(test, map);
+    }
+}
