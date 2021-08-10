31562bd920d5d7ea3c4fc670941d5505050965d8
PoymaLana <marikakopina@gmail.com>
2021-08-10 20:26:56 +0300

    0. Принцип единственной ответственности [#4913 #188277]

---
+++ src/main/java/ru/job4j/ood/srp/SimpleSequenceGenerator.java
@@ -0,0 +1,24 @@
+package ru.job4j.ood.srp;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.Random;
+
+import java.util.List;
+
+public class SimpleSequenceGenerator implements SequenceGenerator<Integer> {
+
+    private final NumberGenerator<Integer> numberGenerator;
+
+    public SimpleSequenceGenerator(NumberGenerator<Integer> numberGenerator) {
+        this.numberGenerator = numberGenerator;
+    }
+
+    @Override
+    public List<Integer> generate(int size) {
+        return IntStream.range(0, size)
+                .map(i -> numberGenerator.generate()).boxed()
+                .collect(Collectors.toList());
+    }
+}
