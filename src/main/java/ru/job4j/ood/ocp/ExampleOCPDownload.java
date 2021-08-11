7222a2e072d3c9bc934b255aa862915f8859214f
PoymaLana <marikakopina@gmail.com>
2021-08-11 20:07:27 +0300

    ocp example1

---
+++ src/main/java/ru/job4j/ood/ocp/ExampleOCPDownload.java
@@ -0,0 +1,16 @@
+package ru.job4j.ood.ocp;
+
+public class ExampleOCPDownload {
+
+    public void downloadAsXmlFile(String filename) {
+
+    }
+
+    public void downloadAsPdf(String filename) {
+
+    }
+
+    public void downloadAsFb2(String filename) {
+
+    }
+}
