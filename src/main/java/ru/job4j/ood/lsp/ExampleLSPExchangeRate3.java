bc9997a8dfe6e47c85587501858d033fdb7abe71
PoymaLana <marikakopina@gmail.com>
2021-08-12 16:24:55 +0300

    3 пример нарушения LSP

---
+++ src/main/java/ru/job4j/ood/lsp/ExampleLSPExchangeRate3.java
@@ -0,0 +1,75 @@
+package ru.job4j.ood.lsp;
+
+public class ExampleLSPExchangeRate3 {
+
+    static class Client {
+        private String name;
+        private String passport;
+        private boolean encumbrance;
+
+        public Client(String name, String passport, boolean encumbrance) {
+            this.name = name;
+            this.passport = passport;
+            this.encumbrance = encumbrance;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getPassport() {
+            return passport;
+        }
+
+        public void setPassport(String passport) {
+            this.passport = passport;
+        }
+
+        public boolean isEncumbrance() {
+            return encumbrance;
+        }
+
+        public void setEncumbrance(boolean encumbrance) {
+            this.encumbrance = encumbrance;
+        }
+    }
+
+    static class Bank {
+        public static void checkEncumbrance(Client client) {
+            if (client.encumbrance) {
+                throw new IllegalArgumentException("Наложено обременение на ваш счет.");
+            }
+        }
+
+        public double getMoney(double money, Client client) {
+            checkEncumbrance(client);
+            if (money < 1) {
+                throw new IllegalArgumentException("Укажите верную сумму");
+            }
+            return money;
+        }
+    }
+
+    static class Terminal extends Bank {
+        @Override
+        public double getMoney(double money, Client client) {
+            if (money < 1) {
+                throw new IllegalArgumentException("Укажите верную сумму");
+            }
+            return money;
+        }
+    }
+
+    static class ThirdRule {
+        public static void main(String[] args) {
+            Client client = new Client("Ivan", "569897", true);
+            Bank bank = new Bank();
+            bank.getMoney(56847, client);
+
+        }
+    }
+}
