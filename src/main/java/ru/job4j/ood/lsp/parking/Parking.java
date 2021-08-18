6eb716e6c92096619dcbc2447903288d1e532de2
PoymaLana <marikakopina@gmail.com>
2021-08-18 16:34:39 +0300

    Интерфейсы парковки

---
+++ src/main/java/ru/job4j/ood/lsp/parking/Parking.java
@@ -0,0 +1,7 @@
+package ru.job4j.ood.lsp.parking;
+/*
+С помощью него в классе CarPark будет распределять машины по парковке.
+ */
+public interface Parking {
+    void parking(Transport transport);
+}
