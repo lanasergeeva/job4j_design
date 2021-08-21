affb9d5950ff8fd0b07c2a09a1ba4382b558fc2a
PoymaLana <marikakopina@gmail.com>
2021-08-21 21:54:43 +0300

    dip пример 1

---
+++ src/main/java/ru/job4j/ood/dip/ExampleDIPOne.java
@@ -0,0 +1,29 @@
+package ru.job4j.ood.dip;
+
+
+public class ExampleDIPOne {
+
+    class Photographer {
+        public Camera camera;
+
+        public Photo newPhoto(String nameImage) {
+            return camera.makePhoto(nameImage);
+        }
+    }
+
+    class Camera {
+       public Photo  makePhoto(String name) {
+           return new Photo(name);
+        }
+    }
+
+    class Photo {
+        private String format;
+        private String name;
+
+        public Photo(String name) {
+            this.format = "DCIM";
+            this.name = name;
+        }
+    }
+}
